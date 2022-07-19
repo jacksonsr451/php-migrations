c36b0ef645c8523d9691e6ae00482fa0106fe368
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-07-19 13:26:45 -0300

    create firs commands help and init

--- src/Commands.php
+++ src/Commands.php
@@ -6,8 +6,38 @@ class Commands
 {
     public static function main(array $args = []): void
     {
-        echo "<pre>";
-        print_r($args);
-        echo "<pre>";
+        self::select($args);
+    }
+
+    private static function select(array $args = []): void
+    {
+        switch ($args[1]) {
+            case 'help':
+                self::runHelp();
+                break;
+            case 'init':
+                self::runInit();
+                break;
+            default:
+                print_r("Pleasse select a option!\n");
+                print_r("help - For more info!");
+                print_r("\n\n");
+                break;
+        }
+    }
+
+    public static function runInit(): void
+    {
+        (new MakeMigration())->init();
+    }
+
+    private static function runHelp(): void
+    {
+        print_r("This is migrations by PHP \n");
+        print_r("Select a option by run \n\n");
+        print_r("init - Initialize migrations folder \n");
+        print_r("init - Initialize migrations folder \n");
+        print_r("init - Initialize migrations folder \n");
+        print_r("\n");
     }
 }
